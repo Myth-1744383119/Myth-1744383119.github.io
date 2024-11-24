85a954dca7c90cc709656e2eeb7b7005de1ab145
Myth-1744383119 <1744383119@qq.com>
2024-11-24 20:12:03 +0800

    first

---
+++ JS/index_form.js
@@ -0,0 +1,13 @@
+function handleFormSubmit(event) {
+    event.preventDefault();
+
+    const name = document.getElementById("name").value;
+    const password = document.getElementById("password").value;
+
+    if (password == "20241124") {
+        window.location.replace("../HTML/home.html");
+    }
+    else {
+        alert(name + "祝你被三楼的梨核砸");
+    }
+}
