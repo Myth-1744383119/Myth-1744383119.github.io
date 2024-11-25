eb8e96b13738c1ddd158be2f0131d4717ef4c7aa
PearPit <1744383119@qq.com>
2024-11-25 12:55:02 +0800

    Update index_form.js

--- JS/index_form.js
+++ JS/index_form.js
@@ -4,8 +4,11 @@ function handleFormSubmit(event) {
     const name = document.getElementById("name").value;
     const password = document.getElementById("password").value;
 
-    if (password == "20241124") {
-        window.location.replace("../HTML/home.html");
+    if (name == "THM" || name == "thm" || name == "陶鸿明") {
+        alert("创前任离合，出神_____。");
+    }
+    else if (password == "20241124") {
+        window.location.replace("get404.html");
     }
     else {
         alert(name + "，祝你被三楼的梨核砸");
